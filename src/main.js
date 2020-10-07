f1654ff0a36c0f438e2741a8d3f03a533bc09141
Salimov <3508880@i.ua>
2020-10-07 20:39:38 +0300

    copy from parcel

---
+++ src/main.js
@@ -0,0 +1,6 @@
+import menuMarkup from './js/menu';
+import refs from './js/refs';
+import onThemeSwitchClick from './js/theme';
+
+refs.menuList.innerHTML = menuMarkup;
+refs.themeSwitch.addEventListener('change', onThemeSwitchClick);
